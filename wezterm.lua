015696b64958da30562c4725d5aeb15f00e2f495
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-06-25 09:42:28 -0300

    wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -2,19 +2,19 @@
 local wezterm = require('wezterm')
 
 -- This table will hold the configuration.
-local config = {}
+local config = {
+    -- font = wezterm.font('SFMono Nerd Font'),
+    color_scheme = 'tokyonight_night',
+    window_background_opacity = 0.7,
+    font_size = 13,
+    line_height = 1.2,
+    cell_width = 0.9,
+    use_fancy_tab_bar = false,
+    force_reverse_video_cursor = true,
+    hide_tab_bar_if_only_one_tab = true,
+    adjust_window_size_when_changing_font_size = true,
+    max_fps = 60,
+    window_decorations = 'RESIZE',
+}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'tokyonight_night'
-config.window_background_opacity = 0.5
-
--- and finally, return the configuration to wezterm
 return config
