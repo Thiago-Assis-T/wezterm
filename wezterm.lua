910d1885cef6179a8db94b5e71ce9bccb5fa04d8
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-09-05 13:59:46 -0300

    a

--- wezterm.lua
+++ wezterm.lua
@@ -1,19 +1,16 @@
--- Pull in the wezterm API
 local wezterm = require('wezterm')
 
--- This table will hold the configuration.
 local config = {
     font = wezterm.font('JetBrainsMono Nerd Font'),
-    color_scheme = 'tokyonight_night',
+    color_scheme = 'Catppuccin Mocha (Gogh)',
     window_background_opacity = 0.7,
-    font_size = 13,
-    line_height = 1.0,
+    font_size = 12,
     cell_width = 1,
     use_fancy_tab_bar = false,
     force_reverse_video_cursor = true,
     hide_tab_bar_if_only_one_tab = true,
     adjust_window_size_when_changing_font_size = true,
-    max_fps = 70,
+    max_fps = 60,
     enable_wayland = true,
     hide_mouse_cursor_when_typing = true,
 }
